b9739d6ec02dabdee12ea4f6aa2e47c0f7f640e6
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-09-15 13:17:51 -0400

    Add project files.

---
+++ Part 2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 7;
+            //int b = 4;
+            //int c = 3;
+            //int d = (a + b) / c;
+            //int e = (a + b) % c;
+            //Console.WriteLine($"quotient: {d}");
+            //Console.WriteLine($"remainder: {e}");
+            //int max = int.MaxValue;
+            //int min = int.MinValue;
+            //Console.WriteLine($"The range of integers is {min} to {max}");
+            //int what = max + 3;
+            //Console.WriteLine($"An example of an overflow: {what}");
+            //double a = 5;
+            //double b = 4;
+            //double c = 2;
+            //double d = (a + b) / c;
+            //Console.WriteLine(d);
+            //double e = 19;
+            //double f = 23;
+            //double g = 8;
+            //double h = (e + f) / g;
+            //Console.WriteLine(h);
+            //double max = double.MaxValue;
+            //double min = double.MinValue;
+            //Console.WriteLine($"The range of double is {min} to {max}");
+            //double third = 1.0 / 3.0;
+            //Console.WriteLine(third);
+            //decimal min = decimal.MinValue;
+            //decimal max = decimal.MaxValue;
+            //Console.WriteLine($"The range of the decimal type is {min} to {max}");
+            //double a = 1.0;
+            //double b = 3.0;
+            //Console.WriteLine(a / b);
+
+            //decimal c = 1.0M;
+            //decimal d = 3.0M;
+            //Console.WriteLine(c / d);
+            double radius = 2.50;
+            double area = (radius * radius) * Math.PI;
+            Console.WriteLine(area);
+
+            Console.ReadLine();
+        }
+    }
+}
